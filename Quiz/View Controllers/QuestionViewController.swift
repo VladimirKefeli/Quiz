8d24e818f3cb0d3d556f2bbf5e20c11fbd63399e
VladimirKefeli <kif997@icloud.com>
2021-01-19 16:15:38 +0300

    Getting a dictionary frequencyOfAnswers

--- Quiz/View Controllers/QuestionViewController.swift
+++ Quiz/View Controllers/QuestionViewController.swift
@@ -16,18 +16,33 @@ class QuestionViewController: UIViewController {
     
     @IBOutlet weak var multipleStackView: UIStackView!
     @IBOutlet var multiLabels: [UILabel]!
+    @IBOutlet var multiSwitches: [UISwitch]!
+    
     
     @IBOutlet weak var rangedStackView: UIStackView!
+    @IBOutlet weak var rangedSlider: UISlider!
     @IBOutlet var rangedLabels: [UILabel]!
     
     @IBOutlet weak var questionProgressView: UIProgressView!
     
+   private var currentAnswers: [Answer] {
+        currentQuestion.answers
+    }
     
+    private var answersChosen = [Answer]() {
+        didSet {
+            print(#line, #function, answersChosen)
+        }
+    }
     
+    private var currentQuestion: Question {
+        Question.all[questionIndex]
+    }
     var questionIndex = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        rangedSlider.maximumValue = 0.99999
         updateUI()
     }
     
@@ -35,10 +50,11 @@ class QuestionViewController: UIViewController {
         
         func updateSingleStack() {
             singleStackView.isHidden = false
-            for button in singleButtons {
+            for (index,button) in singleButtons.enumerated() {
                 button.setTitle(nil, for: [])
+                button.tag = index
             }
-            for (button, answer) in zip(singleButtons, answers) {
+            for (button, answer) in zip(singleButtons, currentAnswers) {
                 button.setTitle(answer.text, for: [])
             }
         }
@@ -48,14 +64,14 @@ class QuestionViewController: UIViewController {
             for label in multiLabels {
                 label.text = nil
             }
-            for (label, answer) in zip(multiLabels, answers) {
+            for (label, answer) in zip(multiLabels, currentAnswers) {
                 label.text = answer.text
             }
         }
         
         func updateRangedStack() {
-            rangedLabels.first?.text = answers.first?.text
-            rangedLabels.last?.text = answers.last?.text
+            rangedLabels.first?.text = currentAnswers.first?.text
+            rangedLabels.last?.text = currentAnswers.last?.text
             rangedStackView.isHidden = false
         }
         
@@ -66,15 +82,14 @@ class QuestionViewController: UIViewController {
         
         
         
-        let question = Question.all[questionIndex]
-        let answers = question.answers
+        
         let totalProgress = Float(questionIndex) / Float(Question.all.count)
         
         navigationItem.title = "Вопрос № \(questionIndex + 1)"
-        questionLabel.text = question.text
+        questionLabel.text = currentQuestion.text
         questionProgressView.setProgress(totalProgress, animated: true)
         
-        switch question.type {
+        switch currentQuestion.type {
         case .single:
             updateSingleStack()
         case .multiple:
@@ -82,11 +97,47 @@ class QuestionViewController: UIViewController {
         case .range:
             updateRangedStack()
         }
-        
-        // TODO: change to segue to results screen
-        questionIndex = (questionIndex + 1) % Question.all.count
-        
+    }
+    func nextQuestion() {
+        questionIndex += 1
+        if questionIndex < Question.all.count {
+        updateUI()
+        } else{
+            performSegue(withIdentifier: "Results Segue", sender: nil)
+        }
     }
     
+    @IBAction func singleButtonPressed(_ sender: UIButton) {
+        let answers = Question.all[questionIndex].answers
+        let index = sender.tag
+        guard 0 <= index && index < answers.count else {
+            return
+        }
+        let answer = answers[index]
+        answersChosen.append(answer)
+        nextQuestion()
+    }
     
+    @IBAction func multiButtonPressed() {
+        for (index, multiSwitch) in multiSwitches.enumerated() {
+            if multiSwitch.isOn && index < currentAnswers.count {
+              let answer = currentAnswers[index]
+                answersChosen.append(answer)
+            }
+        }
+        nextQuestion()
+    }
+    
+    @IBAction func rangedButtonPressed() {
+        let index = Int(round(rangedSlider.value * Float(currentAnswers.count - 1)))
+        if index < currentAnswers.count {
+            let answer = currentAnswers[index]
+            answersChosen.append(answer)
+        }
+        nextQuestion()
+    }
+    
+    @IBSegueAction func resultsSegue(_ coder: NSCoder) -> ResultsViewController? {
+        return ResultsViewController(coder: coder, answersChosen)
+    }
 }
--- Quiz/View Controllers/ResultsViewController.swift
+++ Quiz/View Controllers/ResultsViewController.swift
@@ -8,9 +8,28 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
-
+    
+    let answers: [Answer]
+    
+    init?(coder: NSCoder, _ answers: [Answer]) {
+        self.answers = answers
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("\(#line) \(#function) init(coder:) has not been implemented")
+    }
+    
+    func calculatePersonalityResult() {
+        let frequencyOfAnswers = answers.reduce(into: [:]) { counts, answer in
+            counts[answer.name, default: 0] += 1
+        }
+        print(#line, #function, frequencyOfAnswers)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        calculatePersonalityResult()
 
         // Do any additional setup after loading the view.
     }
